6e37118ca3452fa7b5341170b38000fcdc26d7b1
Jacek Tomaszewski <jacek@jtom.me>
2016-10-18 12:47:33 +0200

    Add osx configuration files

---
+++ osx/export.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+defaults export com.vladalexa.MagicPrefs magicpref.plist
+defaults export com.googlecode.iterm2 iterm2.plist
+defaults export com.divisiblebyzero.Spectacle spectacle.plist
---
+++ osx/import.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+defaults import com.vladalexa.MagicPrefs magicpref.plist
+defaults import com.googlecode.iterm2 iterm2.plist
+defaults import com.divisiblebyzero.Spectacle spectacle.plist
