a7952a7550985149a227e74867fcf077467d440f
Jacek Tomaszewski <jacek@jtom.me>
2017-10-01 11:06:16 +0200

    Update omf

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -6,24 +6,9 @@ if test -f $HOME/.dotfiles/custom/config.fish
   . $HOME/.dotfiles/custom/config.fish
 end
 
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
-
 # Theme
-set fish_theme bobthefish
 set theme_display_user yes
 set default_user slimak
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-set fish_plugins rbenv bundler rails
-
-# Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
-
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
 
 set TERM xterm-256color
 
