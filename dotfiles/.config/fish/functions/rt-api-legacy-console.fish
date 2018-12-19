450e8bf18c5b7317cc97b64ed72c351555dda900
Jacek Tomaszewski <jacek@jtom.me>
2018-12-19 15:17:31 +0100

    fix rt alias

--- dotfiles/.config/fish/functions/rt-api-legacy-console.fish
+++ dotfiles/.config/fish/functions/rt-api-legacy-console.fish
@@ -1,4 +1,4 @@
 # Defined in rt.fish @ line 1
 function rt-api-legacy-console
-  ssh -t core@149.202.184.50 "docker exec -it \$(docker ps | grep recruitee/legacy: | awk 'NR==1{print \$1}') bundle exec rails c production"
+  ssh -t core@149.202.184.50 "docker exec -it \$(docker ps | grep recruitee/legacy | awk 'NR==1{print \$1}') bundle exec rails c production"
 end
