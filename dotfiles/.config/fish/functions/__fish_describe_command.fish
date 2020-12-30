ab2617904f06278f815b162cc4d30473e162274b
Jacek Tomaszewski <jacek@jtom.me>
2020-12-30 19:15:50 +0100

    Make fish prompt faster on os x

---
+++ dotfiles/.config/fish/functions/__fish_describe_command.fish
@@ -0,0 +1,7 @@
+# Disable command descriptions on tab autocomplete
+# to make it faster
+# ( https://github.com/fish-shell/fish-shell/issues/6270 )
+# Note: Above issue says it's on mac os 10.15, but it's slow on 11.1 too .
+function __fish_describe_command
+
+end
